1d645cdc95cc2c75c003720d3f4e8a2170b08c51
KoinoEngineering <email@koino.engineering>
2020-04-15 21:58:41 +0900

    #3 fix Header Title

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -20,13 +20,26 @@ const useIconStyle = makeStyles(
     }
     ));
 
+const useHeaderTitleStyle = makeStyles(({ breakpoints }) =>
+    createStyles({
+        root: {
+            [breakpoints.up("sm")]: {
+                display: "none",
+            }
+        }
+    }
+    ));
+
 const Header: React.FC = () => {
     const classes = useStyles();
     const iconClasses = useIconStyle();
+    const headerTitileClasses = useHeaderTitleStyle();
     return <AppBar id="Header" classes={classes} position="sticky">
         <Grid container direction="row">
-            <Grid item xs>
-                <Typography variant="h1">こいのエンジニアリングのホームページ</Typography>
+            <Grid container item xs>
+                <Grid item>
+                    <Typography variant="h1">こいの<br className={headerTitileClasses.root} />エンジニアリング</Typography>
+                </Grid>
             </Grid>
             <Grid item>
                 <IconButton classes={iconClasses} href={packageJson.repository.url} target="_blank" rel="noopener noreferrer">
