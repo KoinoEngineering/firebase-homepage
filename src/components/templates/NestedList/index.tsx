88c021a10a6462c69c0f8966b2e5f3fefa96d84c
KoinoEngineering <email@koino.engineering>
2020-04-15 21:30:10 +0900

    #3 add profile

---
+++ src/components/templates/NestedList/index.tsx
@@ -0,0 +1,27 @@
+import { List, ListItem } from "@material-ui/core";
+import React from "react";
+import { Propsof } from "src/interfaces/Props";
+import { isString } from "util";
+
+export interface NestedListProps extends Exclude<Propsof<typeof List>, "children"> {
+    items?: NestedListItemProps[];
+}
+
+interface NestedListItemProps extends Omit<Propsof<typeof ListItem>, "button"> {
+    content?: NestedListProps | string;
+}
+
+const NestedList: React.FC<NestedListProps> = ({ items, ...props }) => {
+    return <List {...props}>
+        {
+            items?.map(({ content, ...props }) => <ListItem {...props} button={true}>
+                {isString(content)
+                    ? content
+                    : <NestedList {...content} />
+                }
+            </ListItem>)
+        }
+    </List>;
+};
+
+export default NestedList;
--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PageContainer from "src/components/templates/Page/PageContainer";
+import Profile from "src/components/templates/Profile";
 
 const Top: React.FC = () => {
     return <PageContainer id="Top">
-        ホームページトップ
+        <Profile />
     </PageContainer>;
 };
 
