5cce55067305113828df7638f3c4ee4146ae932c
KoinoEngineering <email@koino.engineering>
2020-04-27 16:45:35 +0900

    #9 add reset

--- src/components/templates/GridItem/index.tsx
+++ src/components/templates/GridItem/index.tsx
@@ -2,5 +2,7 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import { Propsof } from "src/interfaces/Props";
 
-interface GridItemProps extends Propsof<typeof Grid> { }
-export const GridItem: React.FC<GridItemProps> = (props) => <Grid item xs={12} sm={6} md {...props} />;
+interface GridItemProps extends Propsof<typeof Grid> {
+    reset?: boolean;
+}
+export const GridItem: React.FC<GridItemProps> = ({ reset, ...props }) => <Grid item xs={!reset && 12} sm={!reset && 6} md={!reset} {...props} />;
