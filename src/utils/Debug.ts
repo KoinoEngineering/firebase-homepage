04f78038a05eb8e6aa6c77ba3add4c605d3ef21f
KoinoEngineering <email@koino.engineering>
2020-04-28 16:39:34 +0900

    #13 no-console

---
+++ src/utils/Debug.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+type Debug = Pick<Console, "log" | "error">;
+
+
+const Debug: Debug = {
+    log: (...args: any) => process.env.NODE_ENV !== "production" && console.log(...args),
+    error: (...args: any) => process.env.NODE_ENV !== "production" && console.error(...args),
+};
+
+export default Debug;
