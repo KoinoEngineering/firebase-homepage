a7704a6c83ae1f32d3f37124cbdbce36ebce4e43
KoinoEngineering <email@koino.engineering>
2020-05-28 22:43:30 +0900

    #24 change logger tool

--- src/core/ConfigureStore.ts
+++ src/core/ConfigureStore.ts
@@ -1,7 +1,6 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { routerMiddleware } from "connected-react-router";
 import { createBrowserHistory } from "history";
-import { applyMiddleware, createStore, Middleware } from "redux";
-import { createLogger } from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import createRootReducer from "src/reducers";
 import rootSaga from "src/sagas";
@@ -10,29 +9,16 @@ export const history = createBrowserHistory();
 
 const sagaMiddleware = createSagaMiddleware();
 
-interface Saga {
-    saga: Middleware,
-    env?: NodeJS.ProcessEnv["NODE_ENV"][] | "all"
-}
-
-const sagas: Saga[] = [
-    { saga: routerMiddleware(history) },
-    { saga: sagaMiddleware },
-    {
-        saga: createLogger({
-            collapsed: true
-        }), env: ["development", "test"]
-    },
-];
-
-const configureStore = () => {
-    const store = createStore(createRootReducer(history), applyMiddleware(
-        ...sagas
-            .filter(({ env = "all" }) =>
-                env === "all" || env.some(e => e === process.env.NODE_ENV))
-            .map(({ saga }) => saga)));
+export default () => {
+    const store = configureStore({
+        reducer: createRootReducer(history),
+        middleware: [
+            ...getDefaultMiddleware(),
+            routerMiddleware(history),
+            sagaMiddleware,
+        ],
+        devTools: process.env.NODE_ENV === "development"
+    });
     sagaMiddleware.run(rootSaga);
     return store;
 };
-
-export default configureStore;
